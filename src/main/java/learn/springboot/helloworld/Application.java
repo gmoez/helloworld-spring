c57bda6a3bd29300e8020b3b1b7ebf95334a86e9
moezg <moez.gammoua@outlook.fr>
2024-08-26 10:11:58 +0100

    change color in branch feat2

--- src/main/java/learn/springboot/helloworld/Application.java
+++ src/main/java/learn/springboot/helloworld/Application.java
@@ -13,7 +13,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "<h1 style=\"text-align: center; background-color: red;\">Hello World from Main branch</h1>";
+        return "<h1 style=\"text-align: center; background-color: green;\">Hello World from feat2</h1>";
     }
 
     public static void main(String[] args) {
